f8db6cd1d74897d6f3ed881221058001b87e09a5
JIMENOFONSECA <fonseca@arch.ethz.ch>
2018-08-24 18:44:26 +0800

    implementation new figures and data complete

--- analysis/figure_hdd_cdd_evolution_box.py
+++ analysis/figure_hdd_cdd_evolution_box.py
@@ -11,19 +11,20 @@ from configuration import CONFIG_FILE, DATA_HDD_CDD_PLOTS_FOLDER, DATA_RAW_BUILD
 
 
 
-def calc_graph(data_frame, output_path, colors, season):
+def calc_graph(data_frame, output_path, colors_hdd, colors_cdd, season):
 
     fig = tools.make_subplots(rows=1, cols=1)
-    for year in years_to_map:
+
+    for colorhd, colorcd, year in zip(colors_hdd, colors_cdd, years_to_map):
         data = data_frame[data_frame.index == str(year)]
         x = data["City"]
         if season =="heating":
             y = data["HDD_18_5_C"]
-            trace = go.Box(x=x, y=y, name="HDD for year" + str(year), marker=dict(color=colors[0]))
+            trace = go.Box(x=x, y=y, name="HDD for year " + str(year), marker=dict(color=colorhd))
             fig.append_trace(trace, 1, 1)
         else:
             y = data["CDD_18_5_C"]
-            trace = go.Box(x=x, y=y, name="CDD for year" + str(year), marker=dict(color=colors[1]))
+            trace = go.Box(x=x, y=y, name="CDD for year" + str(year), marker=dict(color=colorcd))
             fig.append_trace(trace, 1, 1)
 
     fig['layout'].update(boxmode='group')#, yaxis=dict(range=[0, 3500]))
@@ -31,20 +32,16 @@ def calc_graph(data_frame, output_path, colors, season):
 
     return fig
 
-def main(years_to_map, output_path, future_hdd_cdd_file, analysis_fields, colors, cities):
+def main(years_to_map, output_path, future_hdd_cdd_file, colors_hdd, colors_cdd, cities):
     # get data from every city and transform into data per scenario
     scenarios = future_hdd_cdd_file["Scenario"].unique()
 
     data_final = pd.DataFrame()
     for i, city in enumerate(cities):
-        data_current = pd.read_csv(os.path.join(DATA_RAW_BUILDING_TODAY_HDD_FOLDER, city + ".csv")).set_index(
-            "site_year")
-        HDD_baseline = data_current.loc[2010, "hdd_18.5C"]
-        CDD_baseline = data_current.loc[2010, "cdd_18.5C"]
 
         data_future_HDD_CDD = future_hdd_cdd_file[future_hdd_cdd_file['City'] == city]
-        HDD_baseline = data_future_HDD_CDD.loc[data_future_HDD_CDD["Scenario"]=="A1B_2020"]["HDD_18_5_C"].values[0]
-        CDD_baseline = data_future_HDD_CDD.loc[data_future_HDD_CDD["Scenario"]=="A1B_2020"]["CDD_18_5_C"].values[0]
+        HDD_baseline = data_future_HDD_CDD.loc[data_future_HDD_CDD["Scenario"]=="1990_2010"]["HDD_18_5_C"].values[0]
+        CDD_baseline = data_future_HDD_CDD.loc[data_future_HDD_CDD["Scenario"]=="1990_2010"]["CDD_18_5_C"].values[0]
 
         # heating case
         data_future_HDD_CDD["YEAR"] = [x.split("_", 1)[1] for x in data_future_HDD_CDD["Scenario"].values]
@@ -60,7 +57,7 @@ def main(years_to_map, output_path, future_hdd_cdd_file, analysis_fields, colors
     data_final["CDD_18_5_C"] = (data_final["CDD_18_5_C"]- data_final["TODAY_CDD_18_5_C"])/data_final["TODAY_CDD_18_5_C"]*100
 
     for season in ["heating", "cooling"]:
-        calc_graph(data_final, output_path, colors, season)
+        calc_graph(data_final, output_path, colors_hdd, colors_cdd, season)
 
 
 if __name__ == "__main__":
@@ -69,7 +66,8 @@ if __name__ == "__main__":
     output_path = DATA_HDD_CDD_PLOTS_FOLDER
     future_hdd_cdd_file = pd.read_csv(DATA_RAW_BUILDING_IPCC_SCENARIOS_FILE)
     analysis_fields = ["A1B", "A1B_CDD"] #["B1", "A1B", "A2", "B1_CDD", "A1B_CDD", "A2_CDD"]
-    colors = ["rgb(240,75,91)", "rgb(63,192,194)"] #["rgb(240,75,91)","rgb(246,148,143)","rgb(252,217,210)", "rgb(63,192,194)", "rgb(171,221,222)", "rgb(225,242,242)"]
-    years_to_map = [2020, 2030, 2040, 2050]
+    colors_hdd = ["rgb(255,205,210)", "rgb(239,154,154)", "rgb(229,115,115)", "rgb(239,83,80)", "rgb(244,67,54)","rgb(229,57,53)","rgb(211,47,47)", "rgb(198,40,40)", "rgb(183,28,28)"]
+    colors_cdd = ["rgb(187,222,251)", "rgb(144,202,249)", "rgb(100,181,246)", "rgb(66,165,245)", "rgb(33,150,243)", "rgb(30,136,229)", "rgb(25,118,210)", "rgb(21,101,192)", "rgb(13,71,161)"]
+    years_to_map = [2020, 2030, 2040, 2050, 2060, 2070, 2080, 2090, 2100]
 
-    main(years_to_map, output_path, future_hdd_cdd_file, analysis_fields,colors, cities)
+    main(years_to_map, output_path, future_hdd_cdd_file, colors_hdd, colors_cdd, cities)
